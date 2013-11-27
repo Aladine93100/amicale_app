c79017003a2bd13070cda83adfea090f5c93e532
Aladine <alazaiane@yahoo.fr>
2013-11-27 20:09:24 +0100

    on recommence tout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmicaleApp::Application.config.secret_key_base = 'c8e853451de5a6198b59540f7119b1852dce7b633c606d5e78b7de52c954a5aa1447430a1b59c10888506a71558d57f571e46c6d1e9db4651b14a0b052fac33d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AmicaleApp::Application.config.session_store :cookie_store, key: '_amicale_app_session'
