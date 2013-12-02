db30991a489825c3c94a55a977922240530c994c
Aladine <alazaiane@yahoo.fr>
2013-12-02 22:26:47 +0100

    layout et routes termines

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
 AmicaleApp::Application.config.secret_key_base = 'c8e853451de5a6198b59540f7119b1852dce7b633c606d5e78b7de52c954a5aa1447430a1b59c10888506a71558d57f571e46c6d1e9db4651b14a0b052fac33d'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 AmicaleApp::Application.routes.draw do
+  get "users/new"
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+# You can have the root of your site routed with "root"
+# root 'welcome#index'
+
   resources :microposts
 
   resources :users
@@ -7,7 +15,7 @@ AmicaleApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
